5369c51c4143e7d01209a922264f2080a6cb1abc
Anastasiia Stepanova <nastyaaa19980@gmail.com>
2023-04-16 19:05:46 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [\App\Http\Controllers\MessageController::class, 'index']);
+Route::post('/messages', [\App\Http\Controllers\MessageController::class, 'sendMessage']);
